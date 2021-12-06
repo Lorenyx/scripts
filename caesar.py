fc5bed594e3d1aecca7e30982181f364eab15368
Lore <30599612+Lorenyx@users.noreply.github.com>
2021-12-05 22:48:46 -0600

    Add files via upload

---
+++ caesar.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+from argparse import ArgumentParser
+
+def caeser_cipher(ciphertext: str, shift: int = 13):
+    """Shifts the ciphertext by shift amount"""
+    
+    for letter in ciphertext:
+        BASE = 0 # Starting character for 
+        if letter.isupper():
+            BASE = ord('A') # ascii value of a
+
+        elif letter.islower():
+            BASE = ord('a') # ascii value of A
+    
+        print( chr( (ord(letter) + shift - BASE) % 26 + BASE ), end='' ) #chr((ord(val[i])-rot - 65) % 26 + 65)
+    print()
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+
+    parser.add_argument('ciphertext', type=str)
+    parser.add_argument('--shift', '-s', type=int, default=13)
+
+    args = parser.parse_args()
+    #TODO add range of shift for bruteforcing
+    caeser_cipher(args.ciphertext, args.shift)
